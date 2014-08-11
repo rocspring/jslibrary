cb3462da68fa5f554fafb65f87004ffa6770e5fe
shaopengwang <shaopengwang@sohu-inc.com>
2014-08-11 11:30:25 +0800

    添加JQuery版本的兼容处理

--- common/findJqObjEvents.js
+++ common/findJqObjEvents.js
@@ -5,12 +5,15 @@
 *@param {String} eventName: 希望获取的事件名称
 *
 *@return 传入事件名称时，返回这个事件的函数；否则，返回dom对象上所有的事件
+*
 **/
 
 function findDomEventsInJquery ( dom, eventName ) {
 	if (!dom) return;
 
-	var eventsObj = $._data( dom, 'events' );
+	var versionArr = $.fn.jquery.split('.');
+	//jquery版本号小于1.4的特殊处理
+	var eventsObj = (versionArr[0] < 2 && versionArr[1] < 5) ? $.data( dom, 'events', undefined, true) : $._data( dom, 'events' );
 
 	return !!eventName ? (eventsObj[eventName][0] ? eventsObj[eventName][0] : null) : eventsObj;
 }
