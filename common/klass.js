f8aa16212ef0e21ee92732f5a1ee03dede0d1d98
wshp <wsp000000@163.com>
2014-08-17 23:37:43 +0800

    重新修改继承的实现机制

--- common/klass.js
+++ common/klass.js
@@ -1,63 +1,22 @@
 /*
 *js继承的实现
+*@author: John Resig
+*实现的目标：
+*1.Klass是一个基类，所有的类都是从Klass类继承的
+*2.任何一个新的类都是从现有的类扩展开的，最初始的类是Klass类
+*3.子类可以调用父类的同名方法
 *
 **/
 
-;(function( global, factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		define(factory);
-	} else if( typeof module === 'object' ){ //node.js
-		module.exports = factory();
-	} else{
-		global.class = factory();
-	}
-})( this, function(){
-	'use strict';
+;(function () {
 
-	//单例模式
-	var Klass = {
+	var initializing = true,
+		fnTest = /xyz/.test(function () { 'xyz';}) ? /\b_super\b/ : /.*/;//判断浏览器是否支持函数序列化
 
-		//创建一个对象
-		create : function ( param ) {
-			if ( typeof param === 'function' ) {
-				return new param();
-			} else if ( typeof param === 'object' && param !== null ){
-				var obj = {};
-				return objExtend( obj, param );
-			}
-		},
-
-		//继承的实现
-		extend : function ( child, parent ) {
-			var args = arguments,
-				argsLen = args.length;
-		}
-	};
-
-	/*
-	*两个对象的拷贝
-	*@param: child : 子对象
-	*@param: parent ：父对象
-	*@param: override ：是否重写子对象已有的属性，默认是false
-	**/
-	function objExtend ( child, parent, override ){
-		if ( typeof child !== 'object' || typeof parent !== 'object' ) return;
-
-		if ( !!override ) {
-			for( i in parent){
-				child[i] = parent[i];
-			}
-		}else {
-			for( i in parent ){
-				if( child[i] === undefined ){
-					child[i] = parent[i];
-				}
-			}
-		}
-
-		return child;
+	function Klass (){
+		
 	}
 
+})();
 
-	window.Klass = Klass;
-});
+
