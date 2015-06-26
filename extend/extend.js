985ceb49f8ff41667d33f3ad71ea7d8d55ae26cd
wshp <wsp000000@163.com>
2015-06-26 23:24:11 +0800

    完成extend的实现

--- extend/extend.js
+++ extend/extend.js
@@ -1,16 +1,48 @@
-(function () {
-	var isInit = false;
+(function() {
+	var isInit = false,
+		funcTest = /xyz/.test(function() {
+            xyz;
+        }) ? /\b_super\b/ : /.*/;
 
 	this.Class = function() {};
 
-	Class.extend = function() {
+	Class.extend = function(opts) {
 
+		var _super = this.prototype;
 
+		isInit = true;
 		var newPrototype = new this();
+		isInit = false;
 
+		for (var name in opts) {
+
+			if ( typeof _super[name] === 'function' &&
+				typeof opts[name] === 'function' &&
+				funcTest.test(opts[name]) ) {
+				newPrototype[name] = (function(name, func){
+
+					return function() {
+						var temp = this._super;
+
+						this._super = _super[name];
+
+						var reslt = func.apply(this, arguments);
+
+						this._super = temp;
+
+						return reslt;
+					};
+				})(name, opts[name]);
+			} else {
+				newPrototype[name] = opts[name];
+			}
+
+		}
 
 		function Base() {
-
+			if (!isInit && !!this.init) {
+				this.init.apply(this, arguments);
+			}
 		}
 
 		Base.prototype = newPrototype;
